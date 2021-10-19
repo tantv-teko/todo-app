eedaf68d42dc55c2193f132eeee21cb79e9d5075
r4tt <awatjkim14072001@gmail.com>
2021-10-19 22:46:18 +0700

    db models

--- src/main/resources/db/db.changelog.sql
+++ src/main/resources/db/db.changelog.sql
@@ -1,5 +1,23 @@
 create table note (
-  note_id int primary key,
-  title varchar(255),
-  content varchar(255)
+    note_id int primary key AUTO_INCREMENT,
+    title varchar(255),
+    content varchar(255),
+    color_id int,
+    create_at DateTime,
+    edited_at DateTime
+);
+create table color (
+  	color_id int primary key AUTO_INCREMENT,
+ 	color_name varchar(255),
+  	code varchar(255),
+    is_default char(20)
+);
+create table label (
+  	label_id int primary key AUTO_INCREMENT,
+ 	label_name varchar(255)
+);
+create table label_in_note (
+  	label_in_note_id int primary key AUTO_INCREMENT,
+    label_id int,
+ 	note_id int
 );
